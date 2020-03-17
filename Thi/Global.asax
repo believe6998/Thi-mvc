4b8a06d7c4a3d14541fc290bf79078b47a5a9f31
Nam Phan <namph@meeyland.com>
2020-03-17 20:10:45 +0700

    first commit

---
+++ Thi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Thi.MvcApplication" Language="C#" %>
---
+++ Thi/Models/Employee.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Thi.Models
+{
+    public class Employee
+    {
+        [Key]
+        [Required]
+        public string EmployeeId { get; set; }
+        [Required]
+        public string EmployeeName { get; set; }
+        [Required]
+        public string Department { get; set; }
+        [Required]
+        public double Salary { get; set; }
+    }
+}
